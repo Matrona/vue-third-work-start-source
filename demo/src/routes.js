bf9bf66c2cda2a8d746422d50544c47bf977d9ab
matronka <matronka@mail.ru>
2024-05-16 11:04:14 +0300

    Update

--- demo/src/routes.js
+++ demo/src/routes.js
@@ -1,1 +1,37 @@
-export default []
+export default [
+  {
+    name: 'Counter Base',
+    path: '/counter-0',
+    component: () => import('./demo0/DemoCounter.vue'),
+  },
+  {
+    name: 'Counter 1',
+    path: '/counter-1',
+    component: () => import('./demo1/DemoCounter.vue'),
+  },
+  {
+    name: 'Counter 4',
+    path: '/counter-4',
+    component: () => import('./demo4/DemoCounter.vue'),
+  },
+  {
+    name: 'Counter 5',
+    path: '/counter-5',
+    component: () => import('./demo5/Server.vue'),
+  },
+  {
+    name: 'Counter 6',
+    path: '/counter-6',
+    component: () => import('./demo6/BouncingBall.vue'),
+  },
+  {
+    path: "/component-a",
+    name: "ComponentA",
+    component: () => import("./demo6/ComponentA.vue"),
+  },
+  {
+    path: "/component-b",
+    name: "ComponentB",
+    component: () => import("./demo6/ComponentB.vue"),
+  },
+];
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,30 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { clickOutside } from "./common/directives";
+import { getToken, removeToken } from "@/services/token-manager";
+import { useAuthStore } from "@/stores";
 
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
 
+app.directive("click-outside", clickOutside);
+
 app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+
+// Проверяем, если пользователь уже вошёл в систему
+const token = getToken();
+if (token) {
+  try {
+    const authStore = useAuthStore();
+    await authStore.getMe();
+    await router.push("/");
+  } catch (e) {
+    removeToken();
+    console.log(e);
+  }
+}
